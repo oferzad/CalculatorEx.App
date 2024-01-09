9673797128fd69db2d7dd21f2471411b942116aa
Ofer Zadikario <ofer.zadikario@gmail.com>
2024-01-09 18:09:24 +0200

    add logic for calculation

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,15 +1,17 @@
-﻿namespace CalculatorEx;
+﻿using System.Data;
+using System.Linq.Expressions;
+
+namespace CalculatorEx;
 
 public partial class MainPage : ContentPage
 {
 
-	double current;
-	
+	bool justEvaluated;
 	public MainPage()
 	{
 		InitializeComponent();
 		display.Text = "0";
-		current = 0;
+		justEvaluated = false;
 	}
 
     private void Button_Clicked(object sender, EventArgs e)
@@ -21,18 +23,44 @@ public partial class MainPage : ContentPage
 		{
 			case "AC":
 				display.Text = "0";
-				current = 0;
 				break;
+            case "+/-":
+                display.Text = Evaluate("-1*("+display.Text+")").ToString(); 
+                justEvaluated = false;
+                break;
+            case "=":
+                display.Text = Evaluate(display.Text).ToString();
+                break;
 			default:
-                if (display.Text != "0")
+                if(justEvaluated)
                 {
-                    display.Text = display.Text + buttonText;
-                }
-				else
+                    justEvaluated = false;
                     display.Text = buttonText;
+                }
+                else
+                    if (display.Text != "0")
+                    {
+                        display.Text = display.Text + buttonText;
+                    }
+				    else
+                        display.Text = buttonText;
 
                 break;
         }
     }
+
+    private double Evaluate(string expression)
+    {
+        expression = expression.Replace('x', '*');
+        expression = expression.Replace("%", "/100");
+
+        justEvaluated = true;
+        var table = new DataTable();
+        var column = new DataColumn("expression", typeof(string), expression);
+        table.Columns.Add(column);
+        table.Rows.Add(table.NewRow());
+        return Convert.ToDouble(table.Rows[0]["expression"]);
+    }
 }
+
 
