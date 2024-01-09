084cbeda45df456df8a23fa25e79e545748c367d
Ofer Zadikario <ofer.zadikario@gmail.com>
2024-01-09 13:14:11 +0200

    continue

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,12 +2,37 @@
 
 public partial class MainPage : ContentPage
 {
+
+	double current;
 	
 	public MainPage()
 	{
 		InitializeComponent();
+		display.Text = "0";
+		current = 0;
 	}
 
-	
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+		Button button = (Button)sender;
+		string buttonText = button.Text;
+
+		switch(buttonText)
+		{
+			case "AC":
+				display.Text = "0";
+				current = 0;
+				break;
+			default:
+                if (display.Text != "0")
+                {
+                    display.Text = display.Text + buttonText;
+                }
+				else
+                    display.Text = buttonText;
+
+                break;
+        }
+    }
 }
 
